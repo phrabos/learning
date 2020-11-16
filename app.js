3a9d3fcc42b4d9b0bb36264855d1135c8b0359af
Patrick Hrabos <phrabos@gmail.com>
2020-11-16 15:40:45 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+console.log('hello');
+// Coding Challenge # 1 & 2
+// BMI = mass / height *2 = mass / (height*height). mass in kg height in meters
+
+/* let markWeight = 88;
+let markHeight = 1.69;
+let markBMI = (markWeight / (markHeight * 2)).toFixed(2);
+let johnWeight = 92;
+let johnHeight = 1.95;
+let johnBMI = (johnWeight / (johnHeight * 2)).toFixed(2);
+let markHigherBMI = markBMI > johnBMI;
+
+document.querySelector('.markBMI').innerHTML = `Mark\'s BMI is = ${markBMI}`;
+document.querySelector('.johnBMI').innerHTML = `John\'s BMI is = ${johnBMI}`;
+
+function compareBMI () {
+
+    if (markHigherBMI) {
+        document.querySelector('.summary').innerHTML = `Mark\'s BMI is ${markBMI} ` + `and is higher than John\'s which is ${johnBMI}.`;
+    } else {
+        document.querySelector('.summary').innerHTML = `John\'s BMI is ${johnBMI} ` + `and is higher than Mark\'s which is ${markBMI}.`;
+    }
+};
+compareBMI(); */
+
+// Coding Challenge # 3
+/* let dolphinScore = [96, 108, 89];
+let dolphinScore2 = [97, 112, 101];
+let dolphinScore3 = [97, 112, 101];
+let koalaScore = [88, 91, 110];
+let koalaScore2 = [109, 95, 123];
+let koalaScore3 = [109, 95, 106];
+let dolphinTotal = 0
+let koalaTotal = 0
+dolphinScore = dolphinScore3;
+koalaScore = koalaScore3;
+
+for (let i = 0; i < dolphinScore.length; i++) {
+     dolphinTotal += dolphinScore[i];
+}
+
+for (let i = 0; i < koalaScore.length; i++) {
+    koalaTotal += koalaScore[i];
+}
+
+let dolphinAverage = dolphinTotal / (dolphinScore.length);
+let koalaAverage = koalaTotal / koalaScore.length;
+
+console.log(dolphinAverage, koalaAverage);
+
+if (dolphinAverage > koalaAverage && dolphinAverage >= 100) {
+    console.log("Dolphins win " + dolphinAverage);
+} else if (dolphinAverage < koalaAverage && koalaAverage >= 100){ 
+    console.log("Koalas Win! " + koalaAverage);
+  } else if (koalaAverage === dolphinAverage && dolphinAverage >= 100) {
+    console.log("It\'s a tie!")
+  } else {
+      console.log("No team wins...")
+  } */
+
+  // Coding Challenge # 4
+  
+ /*  let bill = [275, 40, 430];
+  let tip;
+
+  for (let i = 0; i < bill.length; i++){
+    bill[i] >= 50 && bill[i] <= 300 ? tip = bill[i] * .15 : tip = bill[i] * .20;
+    console.log(`The bill is $${bill[i]}, the tip is $${tip}, and the total is $${bill[i] + tip}`);
+  }; */
+
+  // Function Coding Challenge
+
+
+  
+  // Array Coding Challenge
+
+  
